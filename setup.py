61377bc058af1d398bf5a077d8ccdfa85f267b9f
David Cramer <dcramer@gmail.com>
2011-08-24 14:23:27 -0700

    Versioning the package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='decruft',
-    version='1',
+    version='1.0.1',
     description='',
     author='Sharmila.Gopirajan',
     url='http://code.google.com/p/decruft/',
