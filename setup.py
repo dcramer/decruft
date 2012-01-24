dd545f614b819a1f9859caed8605e7f9df502dd4
David Cramer <dcramer@gmail.com>
2012-01-24 15:47:28 -0800

    Bump to 1.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='decruft',
-    version='1.1.0',
+    version='1.1.1',
     description='',
     author='Sharmila.Gopirajan',
     url='http://code.google.com/p/decruft/',
